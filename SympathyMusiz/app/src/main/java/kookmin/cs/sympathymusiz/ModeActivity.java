5485aea72b49c2dc3d4dfc1fcfabddd945d4cd97
JIUNG SHIN <jushin17@gmail.com>
2015-06-05 13:33:57 +0900

    prerpare to upload any file located in device

--- SympathyMusiz/app/src/main/java/kookmin/cs/sympathymusiz/ModeActivity.java
+++ SympathyMusiz/app/src/main/java/kookmin/cs/sympathymusiz/ModeActivity.java
@@ -1,13 +1,13 @@
 package kookmin.cs.sympathymusiz;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 /**
  * Created by seojunkyo on 2015. 6. 5..
@@ -28,17 +28,12 @@ public class ModeActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode);
 
-        list = (ListView) findViewById(R.id.modelist);
-        dbHelper = new DbOpenHelper(this, dbName, null, dbVersion);
+        Button upload = (Button)findViewById(R.id.upload);
 
-        selectDB();
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                cursor.moveToPosition(position);
-                String str = cursor.getString(cursor.getColumnIndex("name"));
-                Toast.makeText(getApplicationContext(), str, Toast.LENGTH_SHORT).show();
+        upload.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(ModeActivity.this, FileSelectionActivity.class);
+                startActivity(intent);
             }
         });
 
