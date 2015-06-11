d165811dbe6ef778e2fd97dafbaf8cc8889f78eb
seojunkyo <seojk2me@gmail.com>
2015-06-11 18:08:13 +0900

    Id name modifiy

--- SympathyMusiz/app/src/main/java/kookmin/cs/sympathymusiz/MainFragment.java
+++ SympathyMusiz/app/src/main/java/kookmin/cs/sympathymusiz/MainFragment.java
@@ -1,13 +1,11 @@
 package kookmin.cs.sympathymusiz;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.CallbackManager;
@@ -58,16 +56,6 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-        Button btn = (Button) view.findViewById(R.id.btn_test);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("http://www.youtube.com/watch?v=" + "Cx47ULJRjfA"));
-                i.setPackage("com.google.android.youtube");
-                startActivity(i);
-            }
-        });
         return view;
     }
 
