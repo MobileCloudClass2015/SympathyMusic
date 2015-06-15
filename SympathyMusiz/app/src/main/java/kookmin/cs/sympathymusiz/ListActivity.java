35f2f86a52c763bf08f9c43dfa538b1f8dd8daf7
seojunkyo <seojk2me@gmail.com>
2015-06-15 17:02:18 +0900

    update

--- SympathyMusiz/app/src/main/java/kookmin/cs/sympathymusiz/ListActivity.java
+++ SympathyMusiz/app/src/main/java/kookmin/cs/sympathymusiz/ListActivity.java
@@ -1,21 +1,31 @@
 package kookmin.cs.sympathymusiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by seojunkyo on 2015. 6. 4..
  */
-public class ListActivity extends FragmentActivity implements View.OnClickListener{
+public class ListActivity extends FragmentActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.play_list);
-    }
 
-    public void onClick(View v) {
-        switch (v.getId()) {
-        }
+
+        ImageButton upload = (ImageButton)findViewById(R.id.upload);
+
+
+        upload.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), FileSelectionActivity.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
 }
